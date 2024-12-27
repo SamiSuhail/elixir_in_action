4ed8d855b42c32fb013993f89e3c0bbe4ef42c74
Sami Suhail <sami.suhail.dev@gmail.com>
2024-12-27 10:00:57 +0200

    7.2.1 Implementing a cache

---
+++ todo_cache/lib/todo/cache.ex
@@ -0,0 +1,22 @@
+defmodule Todo.Cache do
+  use GenServer
+
+  def start(), do: GenServer.start(__MODULE__, nil)
+
+  def server_process(cache_pid, name), do: GenServer.call(cache_pid, {:server_process, name})
+
+  @impl true
+  def init(_), do: {:ok, %{}}
+
+  @impl true
+  def handle_call({:server_process, name}, _, server_processes) do
+    case Map.fetch(server_processes, name) do
+      {:ok, server_process} -> {:reply, server_process, server_processes}
+
+      :error ->
+        {:ok, server_process} = Todo.Server.start()
+        server_processes = Map.put(server_processes, name, server_process)
+        {:reply, server_process, server_processes}
+    end
+  end
+end
