1ef3779f1cea558a58e3f092e904af92da8ac425
Sami Suhail <sami.suhail.dev@gmail.com>
2024-12-28 14:35:12 +0200

    refactor: comprehension for creating workers, single worker_process function on database server

--- todo_cache/lib/todo/database.ex
+++ todo_cache/lib/todo/database.ex
@@ -5,11 +5,25 @@ defmodule Todo.Database do
 
   def start(), do: GenServer.start(__MODULE__, nil, name: __MODULE__)
 
-  def store(key, term), do: GenServer.cast(__MODULE__, {:store, key, term})
+  def store(key, term) do
+    key
+    |> chose_worker()
+    |> Todo.DatabaseWorker.store(key, term)
+  end
 
-  def delete(key), do: GenServer.cast(__MODULE__, {:delete, key})
+  def get(key) do
+    key
+    |> chose_worker()
+    |> Todo.DatabaseWorker.get(key)
+  end
 
-  def get(key), do: GenServer.call(__MODULE__, {:get, key})
+  def delete(key) do
+    key
+    |> chose_worker()
+    |> Todo.DatabaseWorker.delete(key)
+  end
+
+  defp chose_worker(key), do: GenServer.call(__MODULE__, {:chose_worker, key})
 
   @impl true
   def init(_) do
@@ -21,41 +35,17 @@ defmodule Todo.Database do
   def handle_continue(_continue_arg, _state) do
     File.mkdir_p!(@db_folder)
 
-    pids = 0..2
-    |> Enum.map(fn num ->
+    pids = for num <- 0..2, into: %{} do
       {:ok, pid} = Todo.DatabaseWorker.start(@db_folder)
       {num, pid}
-    end)
-    |> Map.new()
+    end
 
     {:noreply, pids}
   end
 
   @impl true
-  def handle_cast({:store, key, term}, pids) do
-    pid = worker_pid(pids, key)
-    Todo.DatabaseWorker.store(pid, key, term)
-
-    {:noreply, pids}
-  end
-
-  def handle_cast({:delete, key}, pids) do
-    pid = worker_pid(pids, key)
-    Todo.DatabaseWorker.delete(pid, key)
-
-    {:noreply, pids}
-  end
-
-  @impl true
-  def handle_call({:get, key}, _, pids) do
-    pid = worker_pid(pids, key)
-    term = Todo.DatabaseWorker.get(pid, key)
-
-    {:reply, term, pids}
-  end
-
-  defp worker_pid(pids, key) do
+  def handle_call({:chose_worker, key}, _, pids) do
     key_hash = :erlang.phash2(key, 3)
-    Map.get(pids, key_hash)
+    {:reply, Map.get(pids, key_hash), pids}
   end
 end
